5fe38d330d3b4aa53b1b4a233116e18825d95ac7
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-01-13 21:00:21 +0000

    Add class State

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Current file: state.py
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State that inherits from BaseModel"""
+
+    name = ''
